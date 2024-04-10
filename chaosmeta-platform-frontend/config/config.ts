dbd951b00b485ff69424c040e30e89552a8dcf26
灯鱼 <kexiezhang@gmail.com>
2024-04-10 15:12:44 +0800

    feat: remove redundant code

--- chaosmeta-platform-frontend/config/config.ts
+++ chaosmeta-platform-frontend/config/config.ts
@@ -27,12 +27,12 @@ export default defineConfig({
   },
   proxy: {
     '/users': {
-      target: 'http://30.230.19.60:8082/',
+      target: 'http://127.0.0.1:8082/',
       changeOrigin: true,
       pathRewrite: { '^/api': '' },
     },
     '/chaosmeta': {
-      target: 'http://30.230.19.60:8082/',
+      target: 'http://127.0.0.1:8082/',
       changeOrigin: true,
       pathRewrite: { '^/api': '' },
     },
